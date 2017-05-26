22d622e1e24607cae6ed06e7cb30431fade55680
jilmarh <sis671.jilmarhgorenaontiveros@gmail.com>
2017-05-26 04:10:24 -0400

    roles

--- client/loggginModules/loginForm.js
+++ client/loggginModules/loginForm.js
@@ -6,6 +6,12 @@ Template.logginForm.events ({
 	"click #close" : function(e){
         e.preventDefault();
         $(".formulario1").css("opacity",0);
+	},
+	"submit form" : function(e){
+
+		Meteor.loginWithPassword(e.target.email.value,e.target.password.value);
+		$(".formulario1").css("opacity",0);
+		return false;
 	}
 });
 Template.mainpage.events ({
--- client/loggginModules/registerForm.js
+++ client/loggginModules/registerForm.js
@@ -5,5 +5,25 @@ Template.registerForm.events({
 	"click #close" : function(e){
 		e.preventDefault();
         $(".formulario1").css("opacity",0);
+	},
+	"submit form" : function(e){
+      var user = {
+      	"username" : e.target.username.value,
+      	"email" : e.target.email.value,
+      	"password" : e.target.password.value,
+      	"profile" : {
+           "nivel" : e.target.nivel.value
+      	   }
+        };
+        /*
+        este codigo es del servidor
+        */
+        Accounts.createUser(user, function(e){
+           if(e == indefined) {
+           	 $(".formulario1").css("opacity",0);
+           	 Meteor.loginWithPassword(user.username,user.password);
+           }     
+        });
+      	return false;  
 	}
 });
