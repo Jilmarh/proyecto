1eb09d2f630a105c7373a6dc2c3bb6e517d34f19
Vistor2012 <vhchoque43qgmail.com>
2017-06-30 06:27:32 -0400

    subir archivo

--- collections/database.js
+++ collections/database.js
@@ -1,3 +1,18 @@
+import { FilesCollection } from 'meteor/ostrio:files';
+  Images = new FilesCollection({
+  collectionName: 'Images',
+  allowClientCode: false, // Disallow remove files from Client
+  onBeforeUpload(file) {
+    // Allow upload files under 10MB, and only in png/jpg/jpeg formats
+    if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
+      return true;
+    } else {
+      return 'Please upload image, with size equal or less than 10MB';
+    }
+  }
+});
+
+  
 PERFIL = new Mongo.Collection("perfil");
 
 ARTICLE = new Mongo.Collection("articles",{
